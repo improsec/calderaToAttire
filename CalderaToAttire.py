c48e5d2fe3d5d353fedf8471121ec3a8afc9fed9
Devel-ubuntu18 <pmk@improsec.com>
2022-03-16 05:16:52 -0700

    Filled out single procedure for testing

--- CalderaToAttire.py
+++ CalderaToAttire.py
@@ -23,41 +23,62 @@ def splitTactics(fulljson):
     #print (resultDict.get('90c2efaa-8205-480d-8bb6-61d90dbaf81b'))
 
 #TODO: This all need to be decided on.
-def getTarget(inp):
+#Shows what target the tactic is being executed on.
+def getTarget(step):
     targetDict = dict()
     targetDict['host'] = "parag00n"
     targetDict['ip'] = "1.1.1.1"
     targetDict['path'] = "PATH=C:"
     targetDict['user'] = "parag00n - improg00n"
 
-def execData(inp):
+#Takes the ability and extract the execution data.
+def execData(step):
     #print(inp)
     execDict = dict()
-    execDict['execution-command'] = inp.get('command')  #TODO: decode base64?
-    execDict['execution-id'] = "solarflare drip" #TODO: figure out what we want here. Hash dict?
+    execDict['execution-command'] = step.get('command')  #TODO: decode base64?
+    execDict['execution-id'] = "parag00n drip" #TODO: figure out what we want here. Hash dict?
     execDict['execution-source'] = "Caldera - Improsec"
     execDict['execution-category'] = { "name" : "Caldera - Improsec", "abbreviation" : "ci"}
     
-    execDict['target'] = getTarget(inp)
+    execDict['target'] = getTarget(step)
 
-    execDict['time-generated'] = inp.get('agent_reported_time')
+    execDict['time-generated'] = step.get('agent_reported_time')
     return execDict
 
-def procs(inp):
-    return
+def steps(step):
+    stepDict = dict()
+    stepDict['command'] = step.get('command') #TODO: decode base64
+    stepDict['executor'] = step.get('executor')
+    stepDict['order'] = "1"         #TODO: fix if multiple steps.
+    stepDict['time-start'] = step.get('agent_reported_time')
+    stepDict['time-stop'] = "unknown"
+    stepDict['output'] = { "content" : "todo", "level" : "no info", "type" : "no info"}
+    return [stepDict]
+
+
+#Multiple abilities in this? Unsure. Atm this just makes a procedure for one step
+#Takes the ability and extracts all procedure data.
+def procs(step):
+    procDict = dict()
+    procDict['procedure-description'] = ""
+    procDict['procedure-id'] = { "type" : "improsec", "id" : "webForKenni"}       #TODO: Fix this?
+    procDict['mitre-technique-id'] = step.get('attack').get('technique_id')
+    procDict['order'] = "1"             #TODO: fix if we want multiple procedures.
+    procDict['steps'] = steps(step)
+    return [procDict]
 
 
 
-def outputJson(inp):
+def outputJson(step):
     #Create dict and inset version, execution data and procedures.
     output = dict()
     output['attire-version'] = "1.1"
-    output['execution-data'] = execData(inp)
-    output['procedures'] = procs(inp)
+    output['execution-data'] = execData(step)
+    output['procedures'] = procs(step)
 
     #Output the json
-    print(json.dumps(output, indent = 4))
-    return 1
+    #print(json.dumps(output, indent = 4))
+    return output
 
     
 
@@ -71,7 +92,9 @@ def main(arg):
     
     for i in dict1:
         #print(dict1[i])
-        outputJson(dict1[i])
+        out = outputJson(dict1[i])
+        out_file = open(i + ".json", "w")
+        json.dump(out, out_file, indent = 4)
         break
 
 
